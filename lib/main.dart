96b2eb48f3444fbab11613fafc5580766a166ae1
sweetandsourkiss <mooul0529@naver.com>
2024-09-30 20:46:25 +0900

    Init

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: const SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding: EdgeInsets.all(
+              Sizes.size14,
+            ),
+            child: Row(
+              children: [
+                Text("hello"),
+                Gaps.h20,
+                Text("hello"),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
